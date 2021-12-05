036f917821c4b087a899f6fa401c73418db8a38a
0xfawkes <0xfawkes@protonmail.com>
2021-12-05 13:13:32 -0600

    add in stables

--- contracts/Curve3PoolLPBridge.sol
+++ contracts/Curve3PoolLPBridge.sol
@@ -28,6 +28,9 @@ contract Curve3PoolLPBridge is IDefiBridge {
     int128 constant BASE_N_COINS = 3;
     int128 constant N_ALL_COINS = N_COINS + BASE_N_COINS - 1;
 
+    address constant DAI_1 = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    address constant USDC_2 = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+    address constant USDT_3 = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
     ICurvePool curvePool; // Update to Curve Pool
 
